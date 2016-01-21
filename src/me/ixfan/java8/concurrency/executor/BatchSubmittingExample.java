1514ebf7ed7ed2f8bf13f33f5766408b338409bb
0xfan <fweiwei.026@gmail.com>
2016-01-21 21:17:51 +0800

    first commit

---
+++ src/me/ixfan/java8/concurrency/executor/BatchSubmittingExample.java
@@ -0,0 +1,13 @@
+package me.ixfan.java8.concurrency.executor;
+
+/**
+ * Created by xfan on 16/1/21.
+ */
+public class BatchSubmittingExample {
+
+    
+
+    public static void main(String[] args) {
+
+    }
+}
